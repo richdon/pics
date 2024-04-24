0247b764926cb868b50e3a00878aa8273a52dc15
Richard Donald <richard_donald92@outlook.com>
2024-04-23 23:07:23 -0400

    child to parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-function App(){
-    return <div>App</div>
+import SearchBar from "./components/SearchBar";
+
+function App() {
+    const handleSubmit = (term) => {
+        console.log('Do a search with', term);
+    }
+    return <SearchBar onSubmit={handleSubmit}/>
 }
+
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,10 @@
-function SearchBar(){
-    return <div>SearchBar</div>
+function SearchBar({onSubmit}){
+    const handleClick = () => {
+        onSubmit('cars')
+    };
+    return <div>
+        <input />
+        <button onClick={handleClick}>Submit</button>
+    </div>
 }
 export default SearchBar;
