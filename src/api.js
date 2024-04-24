61bf3d360a7f586b8db44423287df4643a04b8c1
Richard Donald <richard_donald92@outlook.com>
2024-04-23 22:33:00 -0400

    unsplash api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async () => {
+   const response = await axios.get('https://api.unsplash.com/search/photos', {
+            headers: {
+                Authorization: 'Client-ID access-id'
+            },
+            params: {
+                query: 'cars'
+            }
+        }
+    );
+   console.log(response);
+    return response;
+};
+export default searchImages;
